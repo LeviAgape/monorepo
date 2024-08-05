2f5d0b095ca7581d7230b59e17af7c3f70b5ba7f
Levi Ágape <agapelevi99@gmail.com>
2024-08-05 04:09:53 -0300

    feat: Screens and styles

--- App.tsx
+++ App.tsx
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import FetchButton from './apps/packages/ui/src/ButtonHeader';
+import CreatePost from './apps/packages/layout/src/CreatePost';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.wrapper}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.headerText}>Open up App.tsx to start working on your app!</Text>
+        <View style={styles.containerPost}>
+          <CreatePost />
+        </View>
+        <View style={styles.containerForm}>
+          <FetchButton />
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1, // Ensures the wrapper takes up the full height of the screen
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#421D86', // Background color for the entire scrollable content
+    padding: 16,
+  },
+  containerPost: {
+    width: '30%'
+  },
+  containerForm: {
+    marginTop: 16,
+  },
+  headerText: {
+    marginBottom: 16,
   },
 });
---
+++ apps/packages/ui/src/ButtonHeader.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
+import axios from 'axios';
+
+interface Post {
+  id: string;
+  title: string;
+  description: string;
+}
+
+const FetchButton: React.FC = () => {
+  const [data, setData] = useState<Post[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get<Post[]>('http://localhost:3000/posts');
+      setData(response.data);
+    } catch (error) {
+      setError('Erro ao buscar dados');
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.button}>
+        <Button title="Atualizar posts" onPress={fetchData} />
+      </View>
+      {loading && <Text>Carregando...</Text>}
+      {error && <Text style={styles.error}>{error}</Text>}
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.post}>
+            <Text style={styles.title}>User: {item.title}</Text>
+            <Text>{item.description}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    borderRadius: 30,
+    width: '100%',
+  },
+  post: {
+    marginTop: 24,
+    marginBottom: 16,
+    padding: 16,
+    backgroundColor: '#FFFF',
+    borderRadius: 8,
+    width: '40%',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'justify',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  error: {
+    color: 'red',
+    marginTop: 16,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+});
+
+export default FetchButton;
